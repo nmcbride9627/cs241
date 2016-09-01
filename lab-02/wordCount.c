c47eaf0c6398fca181e57528d04cb26a36b1b6a1
nmcbride9627 <nmcbride9627@unm.edu>
2016-09-01 12:44:43 -0600

    formatting main

---
+++ lab-02/wordCount.c
@@ -0,0 +1,13 @@
+/*********************/
+/* Nathaniel McBride */
+/* September 9, 2016 */
+/* CS-241-002        */
+/*********************/
+
+#include <stdio.h>
+
+void int main(int argc, char const *argv[])
+{
+  /* code */
+  return 0;
+}
