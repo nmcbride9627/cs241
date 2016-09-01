8cea97b2a362bee8bac77cee4aa295bd93dfd9e3
nmcbride9627 <nmcbride9627@unm.edu>
2016-09-01 14:21:08 -0600

    added base code

--- lab-02/wordCount.c
+++ lab-02/wordCount.c
@@ -6,8 +6,32 @@
 
 #include <stdio.h>
 
-void int main(int argc, char const *argv[])
+#define IN 1  /* inside a word */
+#define OUT 0 /* outside a word */
+
+/* count lines, words, and characters in input */
+int main()
 {
-  /* code */
+  int c, nl, nw, nc, state;
+
+  state = OUT;
+  nl = nw = nc = 0;
+  while ((c = getchar()) != EOF){
+    ++nc;
+    if (c == '\n')
+    {
+      ++nl;
+    }
+    if (c == ' ' || c == '\n' || c == '\t')
+    {
+      state = OUT;
+    }
+    else if (state == OUT)
+    {
+      state = IN;
+      ++nw;
+    }
+    printf("%d %d %d\n", nl, nw, nc);
+  }
   return 0;
 }
