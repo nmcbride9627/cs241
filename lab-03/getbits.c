a664514182a63263d65204e0ffccb79fc280957f
nmcbride9627 <nmcbride9627@unm.edu>
2016-09-13 18:34:45 -0600

    test

--- lab-03/getbits.c
+++ lab-03/getbits.c
@@ -63,6 +63,7 @@ int main(int argc, char const *argv[])
       }
       else if(p > bitCount(x))
       {
+        printf("bitcount: %d", bitCount(x));
         printf("Error: number of bits out of range\n");
       }
       else
