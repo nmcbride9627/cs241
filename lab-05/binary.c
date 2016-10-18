63930501b14faa52a5800d5d390180fb7e9b1b90
nmcbride9627 <nmcbride9627@unm.edu>
2016-10-17 21:37:28 -0600

    add init files

---
+++ lab-05/binary.c
@@ -0,0 +1,14 @@
+/**********************/
+/* Nathaniel McBride  */
+/* October 17, 2016   */
+/* CS-241-002         */
+/**********************/
+
+#include <stdio.h>
+#include <string.h>
+#define MAXLINE 1000
+
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
+}
