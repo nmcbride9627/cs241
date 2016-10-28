e349ddbd74da8b04d9c1260cedc9b1ac656d7ad4
nmcbride9627 <nmcbride9627@unm.edu>
2016-10-28 11:00:45 -0600

    add and test

---
+++ lab-06/cipher.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "lcg.h"
+
+void findUniquePrimeFactors(unsigned long n, unsigned long primes[64]);
+int main(int argc, char const *argv[]) {
+  unsigned long primes[64];
+  unsigned long n = 490;
+  int i = 0;
+  findUniquePrimeFactors(n, primes);
+  while(primes[i])
+  {
+    printf("primes[%d] = %lu\n",i, primes[i]);
+  }
+  return 0;
+}
---
+++ lab-06/lcg.c
@@ -0,0 +1,71 @@
+/**********************/
+/* Nathaniel McBride  */
+/* November 4, 2016   */
+/* CS-241-002         */
+/**********************/
+#include "lcg.h"
+
+void findUniquePrimeFactors(unsigned long n, unsigned long primes[64])
+{
+  int i = 0;
+  int testDivisor = 2;
+
+  while(i < 64)
+  {
+    if(testDivisor * testDivisor > n)
+    {
+      if(n != 1)
+      {
+        primes[i] = n;
+        i++;
+      }
+    }
+    if(n / testDivisor == 0)
+    {
+      primes[i] = testDivisor;
+      i++;
+      while(n % testDivisor == 0)
+      {
+        n /= testDivisor;
+      }
+    }
+    else
+    {
+      testDivisor++;
+    }
+  }
+}
+
+struct LinearCongruentialGenerator makeLCG(unsigned long m, unsigned long c)
+{
+  struct LinearCongruentialGenerator lcg;
+  unsigned long primes[64];
+  unsigned long p;
+  int i = 0;
+
+  lcg.x = c;
+  findUniquePrimeFactors(m, primes);
+  while(primes[i])
+  {
+    p *= primes[i];
+    i++;
+  }
+
+  lcg.m = m;
+  lcg.c = c;
+  if(m%4 == 0)
+  {
+    lcg.a = 1 + 2*p;
+  }
+  else
+  {
+    lcg.a = 1 + p;
+  }
+  return lcg;
+}
+
+unsigned long getNextRandomValue(struct LinearCongruentialGenerator* lcg)
+{
+  lcg->x = (lcg->a*lcg->x + lcg->c) % lcg->m;
+  return lcg->x;
+}
