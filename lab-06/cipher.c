4dcc23a36d411ff7ed8f170077fe82f3aa71cd4b
nmcbride9627 <nmcbride9627@unm.edu>
2016-10-28 11:34:31 -0600

    test

--- lab-06/cipher.c
+++ lab-06/cipher.c
@@ -1,7 +1,65 @@
 #include <stdio.h>
 #include "lcg.h"
+void findUniquePrimeFactors(unsigned long n, unsigned long primes[64])
+{
+  int i = 0;
+  int testDivisor = 2;
 
-void findUniquePrimeFactors(unsigned long n, unsigned long primes[64]);
+  while(i < 64)
+  {
+    if(testDivisor * testDivisor > n)
+    {
+      if(n != 1)
+      {
+        primes[i] = n;
+        i++;
+      }
+    }
+    if(n / testDivisor == 0)
+    {
+      primes[i] = testDivisor;
+      i++;
+      while(n % testDivisor == 0)
+      {
+        n /= testDivisor;
+      }
+    }
+    else
+    {
+      testDivisor++;
+    }
+  }
+}
+
+struct LinearCongruentialGenerator makeLCG(unsigned long m, unsigned long c)
+{
+  struct LinearCongruentialGenerator lcg;
+  unsigned long primes[64];
+  unsigned long p;
+  int i = 0;
+
+  lcg.x = c;
+  findUniquePrimeFactors(m, primes);
+  printf("Dunction call\n");
+  while(primes[i])
+  {
+    p *= primes[i];
+    i++;
+  }
+
+  lcg.m = m;
+  lcg.c = c;
+  if(m%4 == 0)
+  {
+    lcg.a = 1 + 2*p;
+  }
+  else
+  {
+    lcg.a = 1 + p;
+  }
+  return lcg;
+}
+
 int main(int argc, char const *argv[]) {
   unsigned long primes[64];
   unsigned long n = 490;
