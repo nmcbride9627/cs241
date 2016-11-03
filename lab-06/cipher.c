95db4a0205d218ca810c00d6b9803ad23de0f581
nmcbride9627 <nmcbride9627@unm.edu>
2016-11-03 14:39:22 -0600

    test

--- lab-06/cipher.c
+++ lab-06/cipher.c
@@ -1,15 +1,82 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 #include "lcg.h"
 
 int main(int argc, char const *argv[]) {
-  int i;
-  struct LinearCongruentialGenerator lcg = makeLCG(2291092019019, 21390123);
-  printf("LCG.m = %lu\n", lcg.m);
-  printf("LCG.a = %lu\n", lcg.a);
-  printf("LCG.c = %lu\n", lcg.c);
-  printf("LCG.x = %lu\n", lcg.x);
-  for(i = 0; i < 100; i++){
-    printf("Next Random value = %lu\n", getNextRandomValue(&lcg));
-  }
-  return 0;
-}
+    int c, i = 0, delimCount = 0, lineCount = 0;
+    char cipherText;
+    char lcg_m[20], lcg_c[20];
+    unsigned long m, c;
+    bool lineStart = true;
+    bool encrypt = true;
+    struct LinearCongruentialGenerator lcg;
+
+    while ((c = getchar()) != EOF)
+    {
+      if(lineStart && (c == 'e' || c == 'd'))
+      {
+        if(c == 'e')
+        {
+          encrypt = true;
+        }
+        if(c == 'd')
+        {
+          encrypt = false;
+        }
+      }
+      else
+      {
+        /*TODO ERROR*/
+      }
+      if(c == ',')
+      {
+        delimCount++;
+      }
+      if(delimCount == 0)
+      {
+        lcg_m[i] = (c - '0');
+        i++;
+        if(i > 20)
+        {
+          i = 0;
+        }
+      }
+      if(delimCount == 1)
+      {
+        lcg_c[i] = (c - '0');
+        i++;
+        if(i > 20)
+        {
+          i = 0;
+        }
+      }
+      if(delimCount == 2)
+      {
+        if(lcg->m == 0)
+        {
+          m = strtoul(lcg_m, NULL, 10);
+          c = strtoul(lcg_c, NULL, 10);
+          lcg = makeLCG(m,c);
+        }
+        if(encrypt)
+        {
+          printf("%5d ", lineCount);
+          while (c != EOF)
+          {
+            if(c == '\n')
+            {
+              delimCount = 0;
+              lineStart = true;
+              lineCount++;
+              break;
+            }
+            cipherText = c ^ (getNextRandomValue(lcg) % 128)
+            printf("%c", cipherText);
+          }
+          printf("\n");
+        }
+
+      }
+
+    }
