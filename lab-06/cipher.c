7f6751a3ca7d55124f53d9ea1077b968bda9d5d5
nmcbride9627 <nmcbride9627@unm.edu>
2016-11-03 13:09:02 -0600

    test

--- lab-06/cipher.c
+++ lab-06/cipher.c
@@ -10,10 +10,6 @@ int main(int argc, char const *argv[]) {
   printf("LCG.x = %lu\n", lcg.x);
   for(i = 0; i < 100; i++){
     printf("Next Random value = %lu\n", getNextRandomValue(&lcg));
-    printf("LCG.m = %lu\n", lcg.m);
-    printf("LCG.a = %lu\n", lcg.a);
-    printf("LCG.c = %lu\n", lcg.c);
-    printf("LCG.x = %lu\n", lcg.x);
   }
   return 0;
 }
