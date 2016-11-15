0c2d5fbe08361a3934c6bf7cc848cdd75391e987
nmcbride9627 <nmcbride9627@unm.edu>
2016-11-15 15:30:45 -0700

    test

---
+++ lab-07/binarytree.c
@@ -0,0 +1,9 @@
+/**********************/
+/* Nathaniel McBride  */
+/* November 14, 2016  */
+/* CS-241-002         */
+/**********************/
+
+#include "binarytree.h"
+#include <stdio.h>
+#include <stdlib.h>
