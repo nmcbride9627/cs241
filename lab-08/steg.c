37d2dd3d2c22bf7b421ff9ea642eab72d5dbf1bd
nmcbride9627 <nmcbride9627@unm.edu>
2016-11-22 23:34:25 -0700

    test

--- lab-08/steg.c
+++ lab-08/steg.c
@@ -131,8 +131,8 @@ int main(int argc, char* argv[])
     }
 
     /* handle end of row padding */
-    fseek(in, rowPadding -1, SEEK_CUR);
-    for(j = 0; j < rowPadding-1; ++j)
+    fseek(in, rowPadding +1, SEEK_CUR);
+    for(j = 0; j < rowPadding +1; ++j)
     {
       putc(0, out);
     }
