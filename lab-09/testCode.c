bb173ca9a4cd3bedb1040e0ef567e6f7cabca574
nmcbride9627 <nmcbride9627@unm.edu>
2016-11-28 17:55:48 -0700

    test

--- lab-09/testCode.c
+++ lab-09/testCode.c
@@ -9,6 +9,6 @@ int main(int argc, char* argv[]) {
   in = fopen(infile, "rb");
   totalNumChars = genFreqArray(in, freqency);
   printFreq(freqency);
-  printf("Total number chars = %d\n", totalNumChars);
+  printf("Total chars = %d\n", totalNumChars);
   return 0;
 }
