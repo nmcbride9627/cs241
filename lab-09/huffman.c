98e22ff386f3a4aa7652658e0c2eec449558e20b
nmcbride9627 <nmcbride9627@unm.edu>
2016-11-28 17:52:12 -0700

    test

--- lab-09/huffman.c
+++ lab-09/huffman.c
@@ -49,13 +49,13 @@ void printFreq(int freqency[256])
   {
     if(freqency[i] != 0)
     {
-      if(freqency[i] <= 32)
+      if(freqency[i] <= ' ')
       {
-        printf("=%5d%10d\n",i,freqency[i]);
+        printf("=%d%10d\n",i,freqency[i]);
       }
       else
       {
-        printf("%5c%10d\n",i,freqency[i]);
+        printf("%c%10d\n",i,freqency[i]);
       }
     }
   }
