94ec7554b70350c30e087337cc8e3c7458e8cf3c
nmcbride9627 <nmcbride9627@unm.edu>
2016-12-07 19:48:49 -0700

    edfwad

--- lab-09/src/huffman.c
+++ lab-09/src/huffman.c
@@ -202,6 +202,7 @@ void encodeFile(FILE* in, FILE* out)
 void decodeFile(FILE* in, FILE* out)
 {
   unsigned long frequency[MAX] = {0};
+  int array[MAX] = {0}, itr = 0;
   unsigned int totalNumChars = genFreqArrayDecode(in, frequency);
   struct HuffHeap* priorityQueue = genPriorityQueue(frequency, totalNumChars);
   struct HuffNode* huffmanTree= genHuffTree(priorityQueue);
