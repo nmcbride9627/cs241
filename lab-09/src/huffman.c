ccf5b237a11d3b6324fa69a42211e58cf6520ad5
nmcbride9627 <nmcbride9627@unm.edu>
2016-12-07 20:06:58 -0700

    fswefs

--- lab-09/src/huffman.c
+++ lab-09/src/huffman.c
@@ -189,9 +189,10 @@ void encodeFile(FILE* in, FILE* out)
   int array[MAX] = {0}, itr = 0;
   unsigned int totalNumChars = genFreqArrayEncode(in, frequency);
   struct HuffHeap* priorityQueue = genPriorityQueue(frequency, totalNumChars);
+
+    printHeap(priorityQueue);
   struct HuffNode* huffmanTree= genHuffTree(priorityQueue);
 
-  printHeap(priorityQueue);
 }
 
 /***************************************************/
